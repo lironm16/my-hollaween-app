44e617c42221bdf1040366dbbddf95d709b2e149
Liron Matitiyahu <lironm16@gmail.com>
2026-09-19 15:53:35 +0000

    Fix PWA reopen: cache-first navigation, remove boot SW bust
    Co-authored-by: lironm16 <lironm16@gmail.com>

--- public/boot.js
+++ public/boot.js
@@ -1,5 +1,4 @@
-/* Kill a stale service worker that cached unstyled HTML. Public path so
-   Preview iframes can run it even when Next.js blocks /_next/*. */
+/* Viewport height sync for mobile browsers / PWA safe areas. */
 (function () {
   function syncAppH() {
     var vv = window.visualViewport;
@@ -14,56 +13,5 @@
   if (window.visualViewport) {
     window.visualViewport.addEventListener("resize", syncAppH);
     window.visualViewport.addEventListener("scroll", syncAppH);
-  }
-})();
-
-(function () {
-  var flag = "hw-sw-bust-v106";
-  var currentShell = "hw-shell-v107";
-  try {
-    if (localStorage.getItem(flag)) return;
-  } catch (e) {}
-
-  if (!("serviceWorker" in navigator)) return;
-
-  navigator.serviceWorker.getRegistrations().then(function (regs) {
-    return Promise.all(
-      regs.map(function (reg) {
-        return reg.unregister();
-      }),
-    ).then(function (results) {
-      var had = results.some(Boolean);
-      if (!("caches" in window)) {
-        if (had) markAndReload(flag);
-        return;
-      }
-      return caches.keys().then(function (keys) {
-        return Promise.all(
-          keys
-            .filter(function (key) {
-              return key.startsWith("hw-shell-") && key !== currentShell;
-            })
-            .map(function (key) {
-              had = true;
-              return caches.delete(key);
-            }),
-        ).then(function () {
-          if (!had) {
-            try {
-              localStorage.setItem(flag, "1");
-            } catch (e) {}
-            return;
-          }
-          markAndReload(flag);
-        });
-      });
-    });
-  });
-
-  function markAndReload(flag) {
-    try {
-      localStorage.setItem(flag, "1");
-    } catch (e) {}
-    location.reload();
   }
 })();
