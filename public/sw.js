1bc9277e1a000cb97c72083786403744f987c9db
Liron Matitiyahu <lironm16@gmail.com>
2026-09-22 09:02:50 +0000

    Bump version to 0.1.12
    Co-authored-by: lironm16 <lironm16@gmail.com>

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 importScripts("/sw-map-tiles.js");
 
-const APP_VERSION = "0.1.9";
-const CACHE = "hw-shell-0.1.9";
+const APP_VERSION = "0.1.12";
+const CACHE = "hw-shell-0.1.12";
 const TILE_CACHE = MapTileCache.TILE_CACHE;
 const PRECACHE = [
   "/offline.html",
