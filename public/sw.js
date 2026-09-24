849d73ba33638e2510732c0abe137843f567df2f
Liron Matitiyahu <lironm16@gmail.com>
2026-09-24 08:45:43 +0000

    Bump version to 0.1.93 for gem hunt deploy
    Co-authored-by: lironm16 <lironm16@gmail.com>

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 importScripts("/sw-map-tiles.js");
 
-const APP_VERSION = "0.1.92";
-const CACHE = "hw-shell-0.1.92";
+const APP_VERSION = "0.1.93";
+const CACHE = "hw-shell-0.1.93";
 const TILE_CACHE = MapTileCache.TILE_CACHE;
 const PRECACHE = [
   "/offline.html",
