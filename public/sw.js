806f123be835b67c439af2aa57fb7855727bb19f
Liron Matitiyahu <lironm16@gmail.com>
2026-09-26 04:45:02 +0000

    chore: sync app version 0.1.164
    Co-authored-by: lironm16 <lironm16@gmail.com>

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 importScripts("/sw-map-tiles.js");
 
-const APP_VERSION = "0.1.163";
-const CACHE = "hw-shell-0.1.163";
+const APP_VERSION = "0.1.164";
+const CACHE = "hw-shell-0.1.164";
 const TILE_CACHE = MapTileCache.TILE_CACHE;
 const PRECACHE = [
   "/offline.html",
